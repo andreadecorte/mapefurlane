33250deb47783965cbb0180b9a8eefd97126fd00
Andrea Decorte <adecorte@gmail.com>
2026-01-14 10:01:19 +0100

    Add ORDER BY

--- scripts/duckdb/all_existing_name_fur.sql
+++ scripts/duckdb/all_existing_name_fur.sql
@@ -1,4 +1,4 @@
 INSTALL spatial;
 LOAD spatial;
 
-COPY (SELECT id, tags['name:fur'] AS 'name:fur', tags['loc_name:fur'] AS 'loc_name:fur', COALESCE(tags['name:it'], tags['name']) AS 'name:it', tags['name:sl'] AS 'name:sl', tags['name:de'] AS 'name:de', tags['place'] AS place, kind FROM ST_ReadOSM('cropped.osm.pbf') WHERE tags['name:fur'] != '' AND tags['place'] != '') TO '/dev/stdout' WITH (FORMAT csv, HEADER);
+COPY (SELECT id, tags['name:fur'] AS 'name:fur', tags['loc_name:fur'] AS 'loc_name:fur', COALESCE(tags['name:it'], tags['name']) AS 'name:it', tags['name:sl'] AS 'name:sl', tags['name:de'] AS 'name:de', tags['place'] AS place, kind FROM ST_ReadOSM('cropped.osm.pbf') WHERE tags['name:fur'] != '' AND tags['place'] != '' ORDER BY "name:fur" ASC) TO '/dev/stdout' WITH (FORMAT csv, HEADER);
