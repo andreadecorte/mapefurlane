7d43e9764c21f620214237b3c9f35cc500687691
Andrea Decorte <adecorte@gmail.com>
2026-01-15 21:36:22 +0100

    add ORDER BY to missing

--- scripts/duckdb/missing_name_fur.sql
+++ scripts/duckdb/missing_name_fur.sql
@@ -4,4 +4,4 @@ LOAD spatial;
 .mode duckbox
 .maxrows 15000
 
-SELECT id, tags['place'] AS place, tags['name'] AS name, kind FROM ST_ReadOSM('cropped.osm.pbf') WHERE tags['name:fur'] IS NULL AND tags['name'] IS NOT NULL AND tags['place'] != '' AND tags['place'] NOT IN ('square');
+SELECT id, tags['place'] AS place, tags['name'] AS name, kind FROM ST_ReadOSM('cropped.osm.pbf') WHERE tags['name:fur'] IS NULL AND tags['name'] IS NOT NULL AND tags['place'] != '' AND tags['place'] NOT IN ('square', 'farm') ORDER BY "name" ASC;
